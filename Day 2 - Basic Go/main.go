cda88f1af98109383bfe1d71ed9fd73e08d2fac1
Your Name <you@example.com>
2023-05-03 08:25:26 +0700

    second commit

---
+++ Day 2 - Basic Go/main.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+// func main() {
+
+// 	// Fundamental pertama : Variabel
+// 	// Tipe Data : {Numeric -> interger (angka bulat) -> Float (angka desimal)}
+// 	// string, Boolean -> True or False
+// 	var age int = -9
+// 	var age2 uint = 23
+// 	var phi float32 = 3.14
+// 	var address string = "Mojokerto"
+// 	var isMan bool = true
+
+// 	fmt.Print(age, age2, phi, address, isMan)
+// }
+// func main() {
+
+// 	// Operator ==> procesor
+// 	alas := 10
+// 	tinggi := 20
+// 	luas := 0.5 * float32(alas) * float32(tinggi)
+
+// 	fmt.Print(luas)
+// }
+// func main() {
+
+// 	// kondisi - struktur kontrol
+// 	age := 30
+
+// 	if age == 17 {
+// 		fmt.Println("seventeen")
+// 	} else if age > 13 || age < 25 {
+// 		fmt.Println("Dewasa")
+// 	} else {
+// 		fmt.Println("invalid")
+// 	}
+// }
+func main() {
+
+	// // loops
+	// fmt.Println("Hello World")
+
+	// for i := 1; i <= 10; i++ {
+	// 	fmt.Println("Hello World")
+	// }
+
+	// ****
+
+	for i := 0; i <= 3; i++ {
+		fmt.Print("*")
+	}
+}
